efe2ac18de9efe75a532215b5ed3cf3c5354c0f7
Elizabeth Fitzgerald <info@maggpress.com>
2017-04-23 15:55:12 -0400

    fonts added

--- wp-content/themes/fitzstudio/functions.php
+++ wp-content/themes/fitzstudio/functions.php
@@ -72,6 +72,69 @@ endif;
 add_action( 'after_setup_theme', 'fitzstudio_setup' );
 
 /**
+ * Register custom fonts.
+ */
+function fitzstudio_fonts_url() {
+	$fonts_url = '';
+
+	/**
+	 * Translators: If there are characters in your language that are not
+	 * supported by Source Sans Pro and PT Serif, translate this to 'off'. Do not translate
+	 * into your own language.
+	 */
+	$source_sans_pro = _x( 'on', 'Source Sans Pro font: on or off', 'fitzstudio' );
+        
+        $pt_serif = _x( 'on', 'PT Serif font: on or off', 'fitzstudio' );
+
+        $font_families = array();
+        
+        
+	if ( 'off' !== $source_sans_pro ) {
+		$font_families[] = 'Source Sans Pro:400,400i,700,900';
+        }
+        
+        if ( 'off' !== $pt_serif ) {
+		$font_families[] = 'PT Serif:400,400i,700,700i';
+        }
+        
+        
+        if ( in_array ( 'on', array($source_sans_pro, $pt_serif))) {
+		
+                $query_args = array(
+                    'family' => urlencode( implode( '|', $font_families ) ),
+                    'subset' => urlencode( 'latin,latin-ext' ),
+		);
+
+		$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	}
+
+	return esc_url_raw( $fonts_url );
+}
+
+
+/**
+ * Add preconnect for Google Fonts.
+ *
+ * @since Twenty Seventeen 1.0
+ *
+ * @param array  $urls           URLs to print for resource hints.
+ * @param string $relation_type  The relation type the URLs are printed.
+ * @return array $urls           URLs to print for resource hints.
+ */
+function fitzstudio_resource_hints( $urls, $relation_type ) {
+	if ( wp_style_is( 'fitzstudio-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
+		$urls[] = array(
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin',
+		);
+	}
+
+	return $urls;
+}
+add_filter( 'wp_resource_hints', 'fitzstudio_resource_hints', 10, 2 );
+
+
+/**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
  * Priority 0 to make it available to lower priority callbacks.
@@ -102,9 +165,13 @@ function fitzstudio_widgets_init() {
 add_action( 'widgets_init', 'fitzstudio_widgets_init' );
 
 /**
- * Enqueue scripts and styles.
+ * Enqueue scripts and styles. 
  */
 function fitzstudio_scripts() {
+        
+        //enqueue Google Fonts: Source Sans Pro and PT Serif
+        wp_enqueue_style( 'fitzstudio-fonts', fitzstudio_fonts_url() );
+    
 	wp_enqueue_style( 'fitzstudio-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'fitzstudio-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
