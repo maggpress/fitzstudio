db378ea7cbe341dc76d04a1bdea2bb5edcf51f85
Elizabeth Fitzgerald <info@maggpress.com>
2017-04-25 16:51:56 -0400

    left sidebar added

--- wp-content/themes/fitzstudio/template-parts/content-page.php
+++ wp-content/themes/fitzstudio/template-parts/content-page.php
@@ -1,43 +1,46 @@
 <?php
 /**
- * Template part for displaying page content in page.php
+ * Template part for displaying posts.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package FitzStudio
  */
-
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
+	
+	<?php
+	if ( has_post_thumbnail() ) { ?>
+	<figure class="featured-image full-bleed">
+		<?php
+		the_post_thumbnail('fitzstudio-full-bleed');
+		?>
+	</figure><!-- .featured-image full-bleed -->
+	<?php } ?>
 
-	<div class="entry-content">
+		
+	<div class="entry-content post-content">
 		<?php
 			the_content();
-
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'fitzstudio' ),
 				'after'  => '</div>',
 			) );
 		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-				edit_post_link(
-					sprintf(
-						/* translators: %s: Name of current post */
-						esc_html__( 'Edit %s', 'fitzstudio' ),
-						the_title( '<span class="screen-reader-text">"', '"</span>', false )
-					),
-					'<span class="edit-link">',
-					'</span>'
-				);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
+	</div><!-- .entry-content .post-content -->
+	
+	<?php
+	get_sidebar( 'page' );
+	?>
+	
+	<?php
+	// If comments are open or we have at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) :
+		comments_template();
+	endif;
+	?>
 </article><!-- #post-## -->
