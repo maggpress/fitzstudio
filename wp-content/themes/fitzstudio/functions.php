74007f3b6b06e63e1a933fc2cb53bed4ef171deb
Elizabeth Fitzgerald <info@maggpress.com>
2017-04-25 08:47:26 -0400

    comments styled

--- wp-content/themes/fitzstudio/functions.php
+++ wp-content/themes/fitzstudio/functions.php
@@ -19,14 +19,12 @@ function fitzstudio_setup() {
 	/*
 	 * Make theme available for translation.
 	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on FitzStudio, use a find and replace
+	 * If you're building a theme based on fitzstudio, use a find and replace
 	 * to change 'fitzstudio' to the name of your theme in all the template files.
 	 */
 	load_theme_textdomain( 'fitzstudio', get_template_directory() . '/languages' );
-
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
-
 	/*
 	 * Let WordPress manage the document title.
 	 * By adding theme support, we declare that this theme does not use a
@@ -34,20 +32,19 @@ function fitzstudio_setup() {
 	 * provide it for us.
 	 */
 	add_theme_support( 'title-tag' );
-
 	/*
 	 * Enable support for Post Thumbnails on posts and pages.
 	 *
 	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 	 */
 	add_theme_support( 'post-thumbnails' );
-
+	add_image_size( 'fitzstudio-full-bleed', 2000, 1200, true );
+	add_image_size( 'fitzstudio-index-img', 1000, 550, true );
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => esc_html__( 'Header', 'fitzstudio' ),
 		'social' => esc_html__( 'Social Media Menu', 'fitzstudio' ),
 	) );
-
 	/*
 	 * Switch default core markup for search form, comment form, and comments
 	 * to output valid HTML5.
@@ -59,67 +56,50 @@ function fitzstudio_setup() {
 		'gallery',
 		'caption',
 	) );
-
 	// Set up the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( 'fitzstudio_custom_background_args', array(
 		'default-color' => 'ffffff',
 		'default-image' => '',
 	) ) );
-        
-        // Add theme support for Custom Logo
+	// Add theme support for Custom Logo
 	add_theme_support( 'custom-logo', array(
 		'width' => 90,
 		'height' => 90,
 		'flex-width' => true,
 	));
-
-	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+	/* Editor styles */
+	add_editor_style( array( 'inc/editor-styles.css', fitzstudio_fonts_url() ) );
 }
 endif;
 add_action( 'after_setup_theme', 'fitzstudio_setup' );
-
 /**
  * Register custom fonts.
  */
 function fitzstudio_fonts_url() {
 	$fonts_url = '';
-
 	/**
 	 * Translators: If there are characters in your language that are not
 	 * supported by Source Sans Pro and PT Serif, translate this to 'off'. Do not translate
 	 * into your own language.
 	 */
 	$source_sans_pro = _x( 'on', 'Source Sans Pro font: on or off', 'fitzstudio' );
-        
-        $pt_serif = _x( 'on', 'PT Serif font: on or off', 'fitzstudio' );
-
-        $font_families = array();
-        
-        
+	$pt_serif = _x( 'on', 'PT Serif font: on or off', 'fitzstudio' );
+	$font_families = array();
 	if ( 'off' !== $source_sans_pro ) {
 		$font_families[] = 'Source Sans Pro:400,400i,700,900';
-        }
-        
-        if ( 'off' !== $pt_serif ) {
+	}
+	if ( 'off' !== $pt_serif ) {
 		$font_families[] = 'PT Serif:400,400i,700,700i';
-        }
-        
-        
-        if ( in_array ( 'on', array($source_sans_pro, $pt_serif))) {
-		
-                $query_args = array(
-                    'family' => urlencode( implode( '|', $font_families ) ),
-                    'subset' => urlencode( 'latin,latin-ext' ),
+	}
+	if ( in_array( 'on', array($source_sans_pro, $pt_serif) ) ) {
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+			'subset' => urlencode( 'latin,latin-ext' ),
 		);
-
 		$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
 	}
-
 	return esc_url_raw( $fonts_url );
 }
-
-
 /**
  * Add preconnect for Google Fonts.
  *
@@ -136,12 +116,9 @@ function fitzstudio_resource_hints( $urls, $relation_type ) {
 			'crossorigin',
 		);
 	}
-
 	return $urls;
 }
 add_filter( 'wp_resource_hints', 'fitzstudio_resource_hints', 10, 2 );
-
-
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
@@ -153,7 +130,69 @@ function fitzstudio_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'fitzstudio_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'fitzstudio_content_width', 0 );
-
+/**
+ * Add custom image sizes attribute to enhance responsive image functionality
+ * for content images.
+ *
+ * @origin Twenty Seventeen 1.0
+ *
+ * @param string $sizes A source size value for use in a 'sizes' attribute.
+ * @param array  $size  Image size. Accepts an array of width and height
+ *                      values in pixels (in that order).
+ * @return string A source size value for use in a content image 'sizes' attribute.
+ */
+function fitzstudio_content_image_sizes_attr( $sizes, $size ) {
+	$width = $size[0];
+	if ( 900 <= $width ) {
+		$sizes = '(min-width: 900px) 700px, 900px';
+	}
+	if ( is_active_sidebar( 'sidebar-1' ) || is_active_sidebar( 'sidebar-2' ) ) {
+		$sizes = '(min-width: 900px) 600px, 900px';
+	}
+	return $sizes;
+}
+add_filter( 'wp_calculate_image_sizes', 'fitzstudio_content_image_sizes_attr', 10, 2 );
+/**
+ * Filter the `sizes` value in the header image markup.
+ *
+ * @origin Twenty Seventeen 1.0
+ *
+ * @param string $html   The HTML image tag markup being filtered.
+ * @param object $header The custom header object returned by 'get_custom_header()'.
+ * @param array  $attr   Array of the attributes for the image tag.
+ * @return string The filtered header image HTML.
+ */
+function fitzstudio_header_image_tag( $html, $header, $attr ) {
+	if ( isset( $attr['sizes'] ) ) {
+		$html = str_replace( $attr['sizes'], '100vw', $html );
+	}
+	return $html;
+}
+add_filter( 'get_header_image_tag', 'fitzstudio_header_image_tag', 10, 3 );
+/**
+ * Add custom image sizes attribute to enhance responsive image functionality
+ * for post thumbnails.
+ *
+ * @origin Twenty Seventeen 1.0
+ *
+ * @param array $attr       Attributes for the image markup.
+ * @param int   $attachment Image attachment ID.
+ * @param array $size       Registered image size or flat array of height and width dimensions.
+ * @return string A source size value for use in a post thumbnail 'sizes' attribute.
+ */
+function fitzstudio_post_thumbnail_sizes_attr( $attr, $attachment, $size ) {
+	if ( !is_singular() ) {
+		if ( is_active_sidebar( 'sidebar-1' ) ) {
+			$attr['sizes'] = '(max-width: 900px) 90vw, 800px';
+		} else {
+			$attr['sizes'] = '(max-width: 1000px) 90vw, 1000px';
+		}
+	} else {
+		$attr['sizes'] = '100vw';
+	}
+	return $attr;
+}
+add_filter( 'wp_get_attachment_image_attributes', 'fitzstudio_post_thumbnail_sizes_attr', 10, 3 );
 /**
  * Register widget area.
  *
@@ -163,7 +202,25 @@ function fitzstudio_widgets_init() {
 	register_sidebar( array(
 		'name'          => esc_html__( 'Sidebar', 'fitzstudio' ),
 		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'fitzstudio' ),
+		'description'   => esc_html__( 'Add sidebar widgets here.', 'fitzstudio' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+	register_sidebar( array(
+		'name'          => esc_html__( 'Page Sidebar', 'fitzstudio' ),
+		'id'            => 'sidebar-2',
+		'description'   => esc_html__( 'Add page sidebar widgets here.', 'fitzstudio' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+	register_sidebar( array(
+		'name'          => esc_html__( 'Footer Widgets', 'fitzstudio' ),
+		'id'            => 'footer-1',
+		'description'   => esc_html__( 'Add footer widgets here.', 'fitzstudio' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
 		'before_title'  => '<h2 class="widget-title">',
@@ -171,53 +228,41 @@ function fitzstudio_widgets_init() {
 	) );
 }
 add_action( 'widgets_init', 'fitzstudio_widgets_init' );
-
 /**
- * Enqueue scripts and styles. 
+ * Enqueue scripts and styles.
  */
 function fitzstudio_scripts() {
-        
-        //enqueue Google Fonts: Source Sans Pro and PT Serif
-        wp_enqueue_style( 'fitzstudio-fonts', fitzstudio_fonts_url() );
-    
+	// Enqueue Google Fonts: Source Sans Pro and PT Serif
+	wp_enqueue_style( 'fitzstudio-fonts', fitzstudio_fonts_url() );
 	wp_enqueue_style( 'fitzstudio-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'fitzstudio-navigation', get_template_directory_uri() . '/js/navigation.js', array('jquery'), '20151215', true );
-        wp_localize_script( 'fitzstudio-navigation', 'fitzstudioScreenReaderText', array(
+	wp_enqueue_script( 'fitzstudio-navigation', get_template_directory_uri() . '/js/navigation.js', array('jquery'), '20170420', true );
+	wp_localize_script( 'fitzstudio-navigation', 'fitzstudioScreenReaderText', array(
 		'expand' => __( 'Expand child menu', 'fitzstudio'),
 		'collapse' => __( 'Collapse child menu', 'fitzstudio'),
 	));
-        
-        wp_enqueue_script( 'fitzstudio-functions', get_template_directory_uri() . '/js/functions.js', array('jquery'), '20170421', true );
-        
-	wp_enqueue_script( 'fitzstudio-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
+	wp_enqueue_script( 'fitzstudio-functions', get_template_directory_uri() . '/js/functions.js', array('jquery'), '20170420', true );
+	wp_enqueue_script( 'fitzstudio-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20170420', true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'fitzstudio_scripts' );
-
 /**
  * Implement the Custom Header feature.
  */
 require get_template_directory() . '/inc/custom-header.php';
-
 /**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
-
 /**
  * Custom functions that act independently of the theme templates.
  */
 require get_template_directory() . '/inc/extras.php';
-
 /**
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
-
 /**
  * Load Jetpack compatibility file.
  */
